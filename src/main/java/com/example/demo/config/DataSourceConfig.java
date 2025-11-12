13b3d3bb287524ea27b777e9571b8b251a633be9
aass1822 <rbals6411@naver.com>
2025-11-12 10:26:00 +0900

    .

--- src/main/java/com/example/demo/config/DataSourceConfig.java
+++ src/main/java/com/example/demo/config/DataSourceConfig.java
@@ -13,7 +13,7 @@ public class DataSourceConfig {
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/testdb");
         dataSource.setUsername("root");
-        dataSource.setPassword("1234");
+        dataSource.setPassword("Zhfldk11!");
         return dataSource;
     }
 
