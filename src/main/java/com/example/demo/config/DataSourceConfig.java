2654758743fe86ddda09d020f3fd95af62f1a2fd
aass1822 <rbals6411@naver.com>
2025-11-13 14:24:06 +0900

    .

--- src/main/java/com/example/demo/config/DataSourceConfig.java
+++ src/main/java/com/example/demo/config/DataSourceConfig.java
@@ -12,7 +12,7 @@ public class DataSourceConfig {
         HikariDataSource dataSource = new HikariDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/testdb");
-        dataSource.setUsername("root");
+        dataSource.setUsername("dbconn");
         dataSource.setPassword("Zhfldk11!");
         return dataSource;
     }
